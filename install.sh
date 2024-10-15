49aa14746bcf6dcce43faac241e7cdaacf503aaa
Alexis Bouchez <alexbcz@proton.me>
2024-10-15 17:33:14 +0200

    replace "software citadel" to "valyent"

--- install.sh
+++ install.sh
@@ -9,40 +9,40 @@ main() {
   arch=$(uname -m)
   version=${1:-$latest_version}
 
-  citadel_dir="${CITADEL_DIR:-$HOME/.citadel}"
+  valyent_dir="${VALYENT_DIR:-$HOME/.valyent}"
 
-	bin_dir="$citadel_dir/bin"
-	tmp_dir="$citadel_dir/tmp"
-	exe="$bin_dir/citadel"
+	bin_dir="$valyent_dir/bin"
+	tmp_dir="$valyent_dir/tmp"
+	exe="$bin_dir/valyent"
 
 	mkdir -p "$bin_dir"
 	mkdir -p "$tmp_dir"
   
   download_url="https://github.com/valyentdev/cli/releases/download/$version/cli_${os}_$arch.tar.gz"
 	echo "Downloading $download_url..."
-  curl -q --fail --location --progress-bar --output "$tmp_dir/citadel.tar.gz" "$download_url"
-	tar -C "$tmp_dir" -xzf "$tmp_dir/citadel.tar.gz"
-	chmod +x "$tmp_dir/citadel"
-	mv "$tmp_dir/citadel" "$exe"
-	rm "$tmp_dir/citadel.tar.gz"
+  curl -q --fail --location --progress-bar --output "$tmp_dir/valyent.tar.gz" "$download_url"
+	tar -C "$tmp_dir" -xzf "$tmp_dir/valyent.tar.gz"
+	chmod +x "$tmp_dir/valyent"
+	mv "$tmp_dir/valyent" "$exe"
+	rm "$tmp_dir/valyent.tar.gz"
 
-	echo "Software Citadel CLI was installed successfully to $exe."
+	echo "Valyent was installed successfully to $exe."
 
-	if command -v citadel >/dev/null; then
-		echo "Run \`citadel auth login\` to get started."
+	if command -v valyent >/dev/null; then
+		echo "Run \`valyent auth login\` to get started."
 	else
 		case $SHELL in
 		/bin/zsh) shell_profile=".zshrc" ;;
 		*) shell_profile=".bash_profile" ;;
 		esac
 
-    echo "\n# Software Citadel CLI" >> "$HOME/$shell_profile"
-    echo "export CITADEL_INSTALL=\"$citadel_dir\"" >> "$HOME/$shell_profile"
-    echo "export PATH=\"\$CITADEL_INSTALL/bin:\$PATH\"" >> "$HOME/$shell_profile"
+    echo "\n# Valyent CLI" >> "$HOME/$shell_profile"
+    echo "export VALYENT_INSTALL=\"$valyent_dir\"" >> "$HOME/$shell_profile"
+    echo "export PATH=\"\$VALYENT_INSTALL/bin:\$PATH\"" >> "$HOME/$shell_profile"
 
-    echo "Open a new terminal or run 'source $HOME/$shell_profile' to start using Software Citadel CLI"
+    echo "Open a new terminal or run 'source $HOME/$shell_profile' to start using Valyent CLI"
 
-		echo "Then, run \`citadel auth login\` to get started."
+		echo "Then, run \`valyent auth login\` to get started."
 	fi
 }
 
