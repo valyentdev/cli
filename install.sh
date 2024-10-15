1aca4ed58ac9d57dba6b3db34c905f57094965e1
Alexis Bouchez <alexbcz@proton.me>
2024-10-15 18:04:42 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@
 set -e
 
 main() {
-  response=$(curl -s "https://api.github.com/repos/valyentdev/cli/releases/latest")
-  latest_version=$(echo "$response" | grep -m 1 '"name":' | awk -F'"' '{print $4}')
-  os=$(uname -s | tr '[:upper:]' '[:lower:]')
+    response=$(curl -s "https://api.github.com/repos/valyentdev/cli/releases/latest")
+    latest_version=$(echo "$response" | grep -m 1 '"name":' | awk -F'"' '{print $4}')
+    latest_version_without_v=${latest_version#v}
 
-  arch=$(uname -m)
-  version=${1:-$latest_version}
+    os=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-  valyent_dir="${VALYENT_DIR:-$HOME/.valyent}"
+    arch=$(uname -m)
+    version=${1:-$latest_version}
+
+    valyent_dir="${VALYENT_DIR:-$HOME/.valyent}"
 
 	bin_dir="$valyent_dir/bin"
 	tmp_dir="$valyent_dir/tmp"
@@ -19,10 +21,11 @@ main() {
 	mkdir -p "$bin_dir"
 	mkdir -p "$tmp_dir"
   
-  download_url="https://github.com/valyentdev/cli/releases/download/$version/valyent_${os}_$arch.tar.gz"
+    download_url="https://github.com/valyentdev/cli/releases/download/$version/valyent_${latest_version_without_v}_${os}_$arch.tar.gz"
 	echo "Downloading $download_url..."
-  curl -q --fail --location --progress-bar --output "$tmp_dir/valyent.tar.gz" "$download_url"
-	tar -C "$tmp_dir" -xzf "$tmp_dir/valyent.tar.gz"
+    curl -q --fail --location --progress-bar --output "$tmp_dir/valyent.tar.gz" "$download_url"
+	
+    tar -C "$tmp_dir" -xzf "$tmp_dir/valyent.tar.gz"
 	chmod +x "$tmp_dir/valyent"
 	mv "$tmp_dir/valyent" "$exe"
 	rm "$tmp_dir/valyent.tar.gz"
@@ -37,13 +40,12 @@ main() {
 		*) shell_profile=".bash_profile" ;;
 		esac
 
-    echo "\n# Valyent CLI" >> "$HOME/$shell_profile"
-    echo "export VALYENT_INSTALL=\"$valyent_dir\"" >> "$HOME/$shell_profile"
-    echo "export PATH=\"\$VALYENT_INSTALL/bin:\$PATH\"" >> "$HOME/$shell_profile"
+        echo "\n# Valyent CLI" >> "$HOME/$shell_profile"
+        echo "export VALYENT_INSTALL=\"$valyent_dir\"" >> "$HOME/$shell_profile"
+        echo "export PATH=\"\$VALYENT_INSTALL/bin:\$PATH\"" >> "$HOME/$shell_profile"
 
-    echo "Open a new terminal or run 'source $HOME/$shell_profile' to start using Valyent CLI"
-
-		echo "Then, run \`valyent auth login\` to get started."
+        echo "Open a new terminal or run 'source $HOME/$shell_profile' to start using Valyent CLI"
+        echo "Then, run \`valyent auth login\` to get started."
 	fi
 }
 
